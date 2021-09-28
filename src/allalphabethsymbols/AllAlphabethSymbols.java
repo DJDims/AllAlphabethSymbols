cea42f0327248fe24292a9d20d4d6c4704cb8143
DJDims <djdims123@mail.ru>
2021-09-28 22:22:37 +0300

    Названия переменных, регистры

--- src/allalphabethsymbols/AllAlphabethSymbols.java
+++ src/allalphabethsymbols/AllAlphabethSymbols.java
@@ -4,22 +4,24 @@ package allalphabethsymbols;
 public class AllAlphabethSymbols {
 
     public static void main(String[] args) {
-        String phrase = "the quick brow fox jumps over the lazy dog";
+        String phrase = "the quick brown fox jumps over the lazy dog";
+
+        phrase = phrase.toLowerCase();
         
-        int counter = 0;
+        int symbolcounter = 0;
         
         char[] alphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
         
         for (int i = 0; i < alphabet.length; i++) {
             for (int j = 0; j < phrase.length(); j++) {
                 if (alphabet[i] == phrase.charAt(j)) {
-                    counter++;
+                    symbolcounter++;
                     break;
                 }
             }
         }
 
-        if (counter == 26) {
+        if (symbolcounter == 26) {
             System.out.println("Строка является панграммой");
         }else{
             System.out.println("Строка не является панграммой");
